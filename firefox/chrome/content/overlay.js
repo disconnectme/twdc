390cb67d3c12b1560511d5fb0619d01e958e2b32
Brian Kennish <byoogle@gmail.com>
2012-02-05 18:04:20 -0800

    Added a location-bar indicator, on-off option, and per-site whitelist.

--- firefox/chrome/content/overlay.js
+++ firefox/chrome/content/overlay.js
@@ -1,7 +1,7 @@
 /*
   An overlay script that stops Twitter from tracking the webpages you go to.
 
-  Copyright 2010, 2011 Disconnect, Inc.
+  Copyright 2010-2012 Disconnect, Inc.
 
   This program is free software: you can redistribute it and/or modify it under
   the terms of the GNU General Public License as published by the Free Software
@@ -18,41 +18,117 @@
   Authors (one per line):
 
     Brian Kennish <byoogle@gmail.com>
+    Gary Teh <garyjob@gmail.com>
 */
 
-/* The XPCOM interfaces. */
-const TWITTER_INTERFACES = Components.interfaces;
+/**
+ * The Twitter Disconnect namespace.
+ */
+if (typeof TwitterDisconnect == 'undefined') {
+  var TwitterDisconnect = {
+    /**
+     * Fetches the number of tracking requests.
+     */
+    getRequestCount: function() {
+      return gBrowser.contentWindow.document.twitterRequestCount;
+    },
 
-/* The domain names Twitter phones home with, lowercased. */
-const TWITTER_DOMAINS = ['twimg.com', 'twitter.com'];
+    /**
+     * Fetches the blocking state.
+     */
+    isUnblocked: function() {
+      return JSON.parse(content.localStorage.twitterUnblocked);
+    },
 
-/*
-  Determines whether any of a bucket of domains is part of a URL, regex free.
-*/
-function isMatching(url, domains) {
-  const DOMAIN_COUNT = domains.length;
-  for (var i = 0; i < DOMAIN_COUNT; i++)
-      if (url.toLowerCase().indexOf(domains[i], 2) >= 2) return true;
-          // A valid URL has at least two characters ("//"), then the domain.
+    /**
+     * Paints the UI.
+     */
+    render: function(that, icon) {
+      var sourceName = 'src';
+      if (that.getRequestCount())
+          icon.setAttribute(
+            sourceName,
+            'chrome://twitter-disconnect/content/' +
+                (that.isUnblocked() ? 'unblocked' : 'blocked') + '.png'
+          );
+      else icon.removeAttribute(sourceName);
+    },
+
+    /**
+     * Navigates to a URL.
+     */
+    go: function(that) { open(that.getAttribute('value'), '_blank'); },
+
+    /**
+     * Registers event handlers.
+     */
+    initialize: function() {
+      var render = this.render;
+      var that = this;
+      var icon = document.getElementById('twitter-disconnect-icon');
+
+      gBrowser.tabContainer.addEventListener('TabAttrModified', function() {
+        render(that, icon);
+      }, false);
+
+      gBrowser.addEventListener('error', function() {
+        render(that, icon);
+      }, false);
+
+      gBrowser.addEventListener('DOMContentLoaded', function() {
+        render(that, icon);
+      }, false);
+
+      icon.onmouseover = function() { this.className = 'highlighted'; };
+
+      icon.onmouseout = function() { this.removeAttribute('class'); };
+
+      var blocking = document.getElementById('twitter-disconnect-blocking');
+
+      icon.onclick = function() {
+        var labelName = 'label';
+        var requestCount = that.getRequestCount() || 0;
+        var label = ' Twitter request';
+        var shortcutName = 'accesskey';
+
+        if (that.isUnblocked()) {
+          blocking.setAttribute(
+            labelName,
+            'Block ' + requestCount + label + (requestCount - 1 ? 's' : '')
+          );
+          blocking.setAttribute(shortcutName, 'B');
+        } else {
+          blocking.setAttribute(
+            labelName,
+            'Unblock ' + requestCount + label + (requestCount - 1 ? 's' : '')
+          );
+          blocking.setAttribute(shortcutName, 'U');
+        }
+      };
+
+      var command = 'command';
+
+      blocking.addEventListener(command, function() {
+        content.localStorage.twitterUnblocked = !that.isUnblocked();
+        content.location.reload();
+      }, false);
+
+      var go = this.go;
+
+      document.
+        getElementById('twitter-disconnect-help').
+        addEventListener(command, function() { go(this); }, false);
+
+      document.
+        getElementById('twitter-disconnect-feedback').
+        addEventListener(command, function() { go(this); }, false);
+    }
+  };
 }
 
-/* Traps and selectively cancels a request. */
-Components.classes['@mozilla.org/observer-service;1']
-  .getService(TWITTER_INTERFACES.nsIObserverService)
-  .addObserver({observe: function(subject) {
-    const NOTIFICATION_CALLBACKS =
-        subject.QueryInterface(
-          TWITTER_INTERFACES.nsIHttpChannel
-        ).notificationCallbacks || subject.loadGroup.notificationCallbacks;
-    const BROWSER =
-        NOTIFICATION_CALLBACKS &&
-            gBrowser.getBrowserForDocument(
-              NOTIFICATION_CALLBACKS
-                .getInterface(TWITTER_INTERFACES.nsIDOMWindow).top.document
-            );
-    subject.referrer.ref;
-        // HACK: The URL read otherwise outraces the window unload.
-    BROWSER && !isMatching(BROWSER.currentURI.spec, TWITTER_DOMAINS) &&
-        isMatching(subject.URI.spec, TWITTER_DOMAINS) &&
-            subject.cancel(Components.results.NS_ERROR_ABORT);
-  }}, 'http-on-modify-request', false);
+/**
+ * Initializes the object.
+ */
+addEventListener('load', function() {
+  TwitterDisconnect.initialize();
+}, false);
---
+++ firefox/components/policy.js
@@ -0,0 +1,101 @@
+/*
+  An XPCOM component that stops Twitter from tracking the webpages you go to.
+
+  Copyright 2010-2012 Disconnect, Inc.
+
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program. If not, see <http://www.gnu.org/licenses/>.
+
+  Authors (one per line):
+
+    Gary Teh <garyjob@gmail.com>
+    Brian Kennish <byoogle@gmail.com>
+*/
+Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
+
+/**
+ * Creates the component.
+ */
+function TwitterDisconnect() { this.wrappedJSObject = this; }
+
+/**
+ * A content policy that stops Twitter from tracking the webpages you go to.
+ */
+TwitterDisconnect.prototype = {
+  /**
+   * The properties required for XPCOM registration.
+   */
+  classID: Components.ID('{2d47d700-93f5-44a5-ae05-7c35a35caf96}'),
+  classDescription:
+      'A content policy that stops Twitter from tracking the webpages you go to.',
+  contractID: '@disconnect.me/twitter;1',
+
+  /**
+   * The categories to register the component in.
+   */
+  _xpcom_categories: [{category: 'content-policy'}],
+
+  /**
+   * Gets a component interface.
+   */
+  QueryInterface:
+      XPCOMUtils.generateQI([Components.interfaces.nsIContentPolicy]),
+
+  /**
+   * The domain names Twitter phones home with, lowercased.
+   */
+  domains: ['twimg.com', 'twitter.com'],
+
+  /**
+   * Determines whether any of a bucket of domains is part of a host name, regex
+   * free.
+   */
+  isMatching: function(host, domains) {
+    var domainCount = domains.length;
+    host = host.toLowerCase();
+    for (var i = 0; i < domainCount; i++)
+        if (host.indexOf(domains[i]) + 1) return true;
+  },
+
+  /**
+   * Traps and selectively cancels a request.
+   */
+  shouldLoad: function(contentType, contentLocation, requestOrigin, context) {
+    var contentPolicy = Components.interfaces.nsIContentPolicy;
+    var isMatching = this.isMatching;
+    var domains = this.domains;
+    var result = contentPolicy.ACCEPT;
+
+    if (
+      contentType != contentPolicy.TYPE_DOCUMENT && // The MIME type.
+          !isMatching(requestOrigin.host, domains) && // The whitelist.
+              isMatching(contentLocation.host, domains) // The blacklist.
+    ) {
+      var html = context.ownerDocument;
+      var twitterRequestCount = html.twitterRequestCount;
+      html.twitterRequestCount =
+          typeof twitterRequestCount == 'undefined' ? 1 :
+              ++twitterRequestCount;
+      if (!JSON.parse(html.defaultView.content.localStorage.twitterUnblocked))
+          result = contentPolicy.REJECT_SERVER; // The blocking state.
+    }
+
+    return result;
+  }
+}
+
+/**
+ * The component entry point.
+ */
+if (XPCOMUtils.generateNSGetFactory)
+    var NSGetFactory = XPCOMUtils.generateNSGetFactory([TwitterDisconnect]);
+else var NSGetModule = XPCOMUtils.generateNSGetModule([TwitterDisconnect]);
